883c00cbebd5d53c15d694f6adafad7e3e6612eb
Kate <katerina.teghai@bk.ru>
2022-08-23 20:50:48 +0300

    first project - movies

--- js/script.js
+++ js/script.js
@@ -1,56 +1,28 @@
-// console.log(1);
-// let number_1 = 13;
-// const NUMBER_2 = 14;
-// console.log(number_1);S
-// number_1 = number_2 + 1;
-// console.log(numbe_r1);  
+"use strict";
 
-// let incr = 10,
-//       decr = 10;
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
 
-// console.log(++incr); 
-// console.log(--decr);
-let a = true,
-    b = true,
-    c = false;
-console.log(a && b && c);
+let personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+let firstFilm = prompt("Один из последних просмотренных фильмов?", ''),
+    firstRate = prompt("На сколько оцените его?", ''),
+    secondFilm = prompt("Ещё один из последних просмотренных фильмов?", ''),
+    secondRate = prompt("На сколько оцените его?", '');
 
-// const answer1 = prompt('What s your name?', '');
-// alert(`Hello, ${answer1}`);
- 
-// alert('Hello bro!');
-// confirm('Do you really want to do this?');
-// const masAnswers = ['answer1'];
+personalMovieDB.movies = {
+    [firstFilm]: +firstRate,
+    [secondFilm]: +secondRate
 
-// const answer2 = prompt('Do you really want to do this?', '');
-// const answer3 = prompt('Do you really want to do this?', '');
-// masAnswers[1] = answer1;
-// masAnswers[2] = answer2;
-// masAnswers[3] = answer3;
-// console.log(masAnswers);
+};
 
-// const person = {
-//     name: "John",
-//     age: 25,
-//     isMarried: false
+// OR
+// personalMovieDB.movies[firstFilm] = firstRate;
+// personalMovieDB.movies[secondFilm] = secondRate;
 
-// }; 
-
-// console.log(person.age);
-
-
-
-// const mas = ['a', 'b', 'c'];
-// const objMas = {
-//     0: 'a',
-//     1: 'b',
-//     2: 'c'
-// };
-
-// console.log(mas[2]);
-// console.log(objMas['0']);
-
-// objMas['3'] = 'd';
-// console.log(objMas);
-
+console.log(personalMovieDB);
