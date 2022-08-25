11095599f931111c3b481b83a3dcaa5a4c17e273
Kate <katerina.teghai@bk.ru>
2022-08-25 18:37:24 +0300

    Условные операторы

---
+++ js/new.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const first = 4;
+
+switch(first){
+    case 0:
+        console.log("Перемненая равна 0");
+        break;
+    case 1:
+        console.log("Переменная равна 1");
+        break;
+    case 2:
+        console.log("Переменная равна 2");
+        break;
+    case 3:
+        console.log("Переменная равна 3");
+        break;
+    default:
+        console.log("Неизвестное значение переменной");
+        break;
+}
+
+
+// if (1){
+//     console.log("Yes, that's true!");
+// } else{
+//     console.log("No, it's bullshit!");
+// }
+// (first <= second) ? console.log("Yeah!") : console.log("Bullshit!");
