ce0ecc5d4c20b224ca6137863db694f8d8fbf210
Kate <katerina.teghai@bk.ru>
2022-08-25 20:27:47 +0300

    Условные операторы 1.0

--- js/new.js
+++ js/new.js
@@ -1,24 +1,24 @@
 "use strict";
 
-const first = 4;
+// const first = 4;
 
-switch(first){
-    case 0:
-        console.log("Перемненая равна 0");
-        break;
-    case 1:
-        console.log("Переменная равна 1");
-        break;
-    case 2:
-        console.log("Переменная равна 2");
-        break;
-    case 3:
-        console.log("Переменная равна 3");
-        break;
-    default:
-        console.log("Неизвестное значение переменной");
-        break;
-}
+// switch(first){
+//     case 0:
+//         console.log("Перемненая равна 0");
+//         break;
+//     case 1:
+//         console.log("Переменная равна 1");
+//         break;
+//     case 2:
+//         console.log("Переменная равна 2");
+//         break;
+//     case 3:
+//         console.log("Переменная равна 3");
+//         break;
+//     default:
+//         console.log("Неизвестное значение переменной");
+//         break;
+// }
 
 
 // if (1){
@@ -27,3 +27,20 @@ switch(first){
 //     console.log("No, it's bullshit!");
 // }
 // (first <= second) ? console.log("Yeah!") : console.log("Bullshit!");
+
+console.log( NaN || 2 || undefined );
+ 
+console.log( NaN && 2 && undefined );
+ 
+console.log( 1 && 2 && 3 );
+ 
+console.log( !1 && 2 || !3 );
+ 
+console.log( 25 || null && !3 );
+ 
+console.log( NaN || null || !3 || undefined || 5);
+ 
+console.log( NaN || null && !3 && undefined || 5);
+ 
+console.log( 5 === 5 && 3 > 1 || 5);
+console.log(true && !4);
