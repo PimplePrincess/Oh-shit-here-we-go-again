bbd490a4cbe3906947d4f9ff9a663a90232cefdb
Kate <katerina.teghai@bk.ru>
2022-08-27 23:57:26 +0300

    Practice

--- js/new.js
+++ js/new.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Рисунок треугольника
 const lenght = 6,
       height = 6;
 
@@ -19,4 +20,36 @@ for(let i = 0; i < height; i++){ //столбец
 }    
 
 
+for(let i = 20; i >= 10; i--){
+    if(i === 13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+    
+}
 
+for(let i = 2; i <= 10; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+
+for (let i = 2; i <= 16; i++) {
+    while(i % 2 !== 0){
+        console.log(i);
+        break;
+    }
+}
+
+const arrayOfNumbers = [];
+for (let i = 5; i <= 10; i++){
+    arrayOfNumbers [i - 5] = i;
+       
+
+      
+    
+} 
+console.log(arrayOfNumbers);
