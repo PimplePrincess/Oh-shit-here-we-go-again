0ca43b2295e7a4622d8bf17821b3e88e6f74e3b5
Kate <katerina.teghai@bk.ru>
2022-08-27 18:38:52 +0300

    Треугольник

--- js/new.js
+++ js/new.js
@@ -1,46 +1,22 @@
 "use strict";
 
-// const first = 4;
+const lenght = 6,
+      height = 6;
 
-// switch(first){
-//     case 0:
-//         console.log("Перемненая равна 0");
-//         break;
-//     case 1:
-//         console.log("Переменная равна 1");
-//         break;
-//     case 2:
-//         console.log("Переменная равна 2");
-//         break;
-//     case 3:
-//         console.log("Переменная равна 3");
-//         break;
-//     default:
-//         console.log("Неизвестное значение переменной");
-//         break;
-// }
+let stroka = '';
+
+for(let i = 0; i < height; i++){ //столбец
+    for(let j = 0;  j < lenght; j++){ //строка
+     if (j <= i){
+        stroka += '*';
+        break;
+     }
+     else if( i < j ){
+        continue;
+     }
+    }
+    console.log(stroka);
+}    
 
 
-// if (1){
-//     console.log("Yes, that's true!");
-// } else{
-//     console.log("No, it's bullshit!");
-// }
-// (first <= second) ? console.log("Yeah!") : console.log("Bullshit!");
 
-console.log( NaN || 2 || undefined );
- 
-console.log( NaN && 2 && undefined );
- 
-console.log( 1 && 2 && 3 );
- 
-console.log( !1 && 2 || !3 );
- 
-console.log( 25 || null && !3 );
- 
-console.log( NaN || null || !3 || undefined || 5);
- 
-console.log( NaN || null && !3 && undefined || 5);
- 
-console.log( 5 === 5 && 3 > 1 || 5);
-console.log(true && !4);
