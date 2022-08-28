d7d02245c6a845fd490a59e3d530a0a65a027c47
Kate <katerina.teghai@bk.ru>
2022-08-29 01:36:32 +0300

    Сложное задание на треуг-

--- js/new.js
+++ js/new.js
@@ -20,6 +20,7 @@ for(let i = 0; i < height; i++){ //столбец
 }    
 
 
+
 for(let i = 20; i >= 10; i--){
     if(i === 13){
         break;
@@ -47,9 +48,66 @@ for (let i = 2; i <= 16; i++) {
 const arrayOfNumbers = [];
 for (let i = 5; i <= 10; i++){
     arrayOfNumbers [i - 5] = i;
-       
-
-      
-    
 } 
 console.log(arrayOfNumbers);
+
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.length; i++){
+    result[i] = arr[i];
+}
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++){
+    if(typeof(data[i]) === 'string'){
+        data[i] +=' - done';
+    } else{
+        data[i] = data[i] * 2;
+    }
+}
+
+
+const data1 = [5, 10, 'Shopping', 20, 'Homework'];
+const result1 = [];
+
+for (let i = 0; i < data1.length; i++){
+    result1[data1.length - i - 1] = data1[i];
+}
+console.log(result1);
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+const lines = 5;
+let result2 = '';
+
+for (let i = 0; i <= lines; i++){
+    for (let j = 0; j <= lines * 2 + 2; j++){
+        if( j === (lines * 2 + 2) / 2){
+            result2 += '*\n';
+            break;
+        } 
+        else if(j >= ((lines * 2 + 2) / 2 - i) && j <= ((lines * 2 + 2) / 2 + i) && j != lines * 2 + 2 / 2){
+            result2 += '*';
+            
+        }
+        else if(j < ((lines * 2 + 2) / 2 - i)){
+            result2 += ' ';
+        }
+        else if(j > ((lines * 2 + 2) / 2 + i)){
+            result2 += '\n';
+        }
+        else{
+            continue;
+        }
+    }
+    console.log(result2);
+}
